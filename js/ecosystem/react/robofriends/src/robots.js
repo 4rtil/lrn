8db30a0810115e8bed556393d31aaa5e7b138c87
Artur <king@DESKTOP-GLBON6K.home>
2021-03-09 22:52:55 +0100

    robofriends app init

---
+++ js/ecosystem/react/robofriends/src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hakuna Matata',
+        username: 'Hakmat',
+        email: 'hakuna@matata.af'
+    },
+    {
+        id: 2,
+        name: 'Bonkers Honkers',
+        username: 'Bonhon',
+        email: 'bon@hon.co.uk'
+    },
+    {
+        id: 3,
+        name: 'Kromulon Friedmanson',
+        username: 'kromulon69',
+        email: 'dooopa@stoneshit.pl'
+    }
+]
