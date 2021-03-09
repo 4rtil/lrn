14d93c7b492ed5eb0098f222913366652fc3b0c4
Artur <king@DESKTOP-GLBON6K.home>
2021-03-09 22:53:20 +0100

    updates

--- js/ecosystem/nodejs/server/app.js
+++ js/ecosystem/nodejs/server/app.js
@@ -2,12 +2,12 @@ const http = require('http');
 const chalk = require('chalk');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(JSON.stringify(process.env));
+  res.end(JSON.stringify(process.env, null, 2));
 });
 
 server.listen(port, hostname, () => {
