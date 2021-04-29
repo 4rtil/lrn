c99f399ed97812c64a41a6283c7f44961de8ba5c
4rtil <artil87@gmail.com>
2021-04-29 13:27:04 +0200

    alternative async/await call to fetch people

--- js/ecosystem/testing/udemy-test/script.async.js
+++ js/ecosystem/testing/udemy-test/script.async.js
@@ -12,4 +12,15 @@ const getPeoplePromise = fetch => {
     })
 }
 
-getPeoplePromise(fetch);
+const getPeople = async (fetch) => {
+    const getRequest = await fetch('http://swapi.py4e.com/api/people/')
+    const data = await getRequest.json();
+    console.log(data);
+    return {
+        count: data.count,
+        results: data.results
+    }
+}
+
+getPeople(fetch);
+//getPeoplePromise(fetch);
